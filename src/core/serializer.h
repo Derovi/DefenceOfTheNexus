56ad17ef6099b6936ccae26c4ba065441fadd90d
programmer228 <i_like_poker@gmail.com>
2020-04-05 12:30:37 +0300

    Static changed
    Changed static of all my methods Serializer class

--- src/core/serializer.h
+++ src/core/serializer.h
@@ -22,66 +22,66 @@ namespace core {
 
 class Serializer {
   public:
-    static void serialize(const core::Object& object, QString& serialized);
+    void serialize(const core::Object& object, QString& serialized);
 
-    static void serialize(const core::Resource& object, QString& serialized);
+    void serialize(const core::Resource& object, QString& serialized);
 
-    static void serialize(const core::ResourceBundle& object, QString& serialized);
+    void serialize(const core::ResourceBundle& object, QString& serialized);
 
-    static void serialize(const core::Moving& object, QString& serialized);
+    void serialize(const core::Moving& object, QString& serialized);
 
-    static void serialize(const core::GameWorld& object, QString& serialized);
+    void serialize(const core::GameWorld& object, QString& serialized);
 
-    static void serialize(const core::Damaging& object, QString& serialized);
+    void serialize(const core::Damaging& object, QString& serialized);
 
-    static void serialize(const core::Building& object, QString& serialized);
+    void serialize(const core::Building& object, QString& serialized);
 
-    static void serialize(const core::Damageable& object, QString& serialized);
+    void serialize(const core::Damageable& object, QString& serialized);
 
-    static void serialize(const core::Unit& object, QString& serialized);
+    void serialize(const core::Unit& object, QString& serialized);
 
-    static bool deserialize(core::Object& object, const QString& serialized);
+    bool deserialize(core::Object& object, const QString& serialized);
 
-    static bool deserialize(core::Resource& object, const QString& serialized);
+    bool deserialize(core::Resource& object, const QString& serialized);
 
-    static bool deserialize(core::ResourceBundle& object, const QString& serialized);
+    bool deserialize(core::ResourceBundle& object, const QString& serialized);
 
-    static bool deserialize(core::Moving& object, const QString& serialized);
+    bool deserialize(core::Moving& object, const QString& serialized);
 
-    static bool deserialize(core::GameWorld& object, const QString& serialized);
+    bool deserialize(core::GameWorld& object, const QString& serialized);
 
-    static bool deserialize(core::Damaging& object, const QString& serialized);
+    bool deserialize(core::Damaging& object, const QString& serialized);
 
-    static bool deserialize(core::Building& object, const QString& serialized);
+    bool deserialize(core::Building& object, const QString& serialized);
 
-    static bool deserialize(core::Damageable& object, const QString& serialized);
+    bool deserialize(core::Damageable& object, const QString& serialized);
 
-    static bool deserialize(core::Unit& object, const QString& serialized);
+    bool deserialize(core::Unit& object, const QString& serialized);
 
   private:
-    static void addJsonObject(const core::Object& object, QJsonObject& json);
+    void addJsonObject(const core::Object& object, QJsonObject& json);
 
-    static void addJsonResource(const core::Resource& object, QJsonObject& json);
+    void addJsonResource(const core::Resource& object, QJsonObject& json);
 
-    static void addJsonDamaging(const core::Damaging& object, QJsonObject& json);
+    void addJsonDamaging(const core::Damaging& object, QJsonObject& json);
 
-    static void addJsonDamageable(const core::Damageable& object, QJsonObject& json);
+    void addJsonDamageable(const core::Damageable& object, QJsonObject& json);
 
-    static void addJsonMoving(const core::Moving& object, QJsonObject& json);
+    void addJsonMoving(const core::Moving& object, QJsonObject& json);
 
-    static void convertJsonToString(const QJsonObject& Object, QString& serialized);
+    void convertJsonToString(const QJsonObject& Object, QString& serialized);
 
-    static bool objectFromString(const QString& in, QJsonObject& json);
+    bool objectFromString(const QString& in, QJsonObject& json);
 
-    static bool buildObject(core::Object& object, const QJsonObject& json);
+    bool buildObject(core::Object& object, const QJsonObject& json);
 
-    static bool buildResourse(core::Resource& object, const QJsonObject& json);
+    bool buildResourse(core::Resource& object, const QJsonObject& json);
 
-    static bool buildDamaging(core::Damaging& object, const QJsonObject& json);
+    bool buildDamaging(core::Damaging& object, const QJsonObject& json);
 
-    static bool buildDamageable(core::Damageable& object, const QJsonObject& json);
+    bool buildDamageable(core::Damageable& object, const QJsonObject& json);
 
-    static bool buildMoving(core::Moving& object, const QJsonObject& json);
+    bool buildMoving(core::Moving& object, const QJsonObject& json);
 };
 
 }  // namespace core
