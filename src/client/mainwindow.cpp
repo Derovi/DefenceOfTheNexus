d6e6ab9f73f080377cfa88cf306d962ebe1f836d
derovi <dirbraindrv@gmail.com>
2020-04-16 16:12:51 +0300

    Window Manager added

---
+++ src/client/mainwindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by derovi on 4/16/2020.
+//
+
+#include "mainwindow.h"
---
+++ src/client/windowmanager.h
@@ -0,0 +1,29 @@
+#ifndef WINDOWMANAGER_H
+#define WINDOWMANAGER_H
+
+class WindowManager {
+  public:
+    WindowManager(int width, int height);
+
+    // coverts from 4k to current display size
+    int get_real_x(int x4k);
+
+    int get_real_y(int y4k);
+
+    int get_x4k(int x);
+
+    int get_y4k(int y);
+
+    void update(int width, int height);
+
+  private:
+    int window_height;
+    int window_width;
+
+    const int width4k = 3840;
+    const int height4k = 2160;
+
+    friend class MainWindow;
+};
+
+#endif // WINDOWMANAGER_H
