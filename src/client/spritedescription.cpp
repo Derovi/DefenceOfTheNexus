6c98924f2c3158d7e3ea1da06880fe2baf9351be
derovi <dirbraindrv@gmail.com>
2020-04-18 23:18:53 +0300

    Sprites added to Factory

---
+++ src/client/spritedescription.cpp
@@ -0,0 +1,5 @@
+//
+// Created by derovi on 4/18/2020.
+//
+
+#include "spritedescription.h"
---
+++ src/client/spritedescription.h
@@ -0,0 +1,14 @@
+//
+// Created by derovi on 4/18/2020.
+//
+
+#ifndef SPRITEDESCRIPTION_H
+#define SPRITEDESCRIPTION_H
+
+
+class SpriteDescription {
+
+};
+
+
+#endif //SPRITEDESCRIPTION_H
