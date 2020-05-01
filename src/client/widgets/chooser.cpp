9467cb670d2ff421728250d4c46685c70a8fa073
derovi <devderovi@gmail.com>
2020-05-01 16:02:26 +0300

    Lang fixed

---
+++ src/client/widgets/chooser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by derovi on 5/1/2020.
+//
+
+#include "chooser.h"
---
+++ src/client/widgets/chooser.h
@@ -0,0 +1,14 @@
+//
+// Created by derovi on 5/1/2020.
+//
+
+#ifndef CHOOSER_H
+#define CHOOSER_H
+
+
+class Chooser {
+
+};
+
+
+#endif //CHOOSER_H
