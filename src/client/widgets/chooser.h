da57104a0385a7b298d429dcb5efd75397407af1
derovi <devderovi@gmail.com>
2020-05-01 16:32:20 +0300

    Chooser widget created

--- src/client/widgets/chooser.h
+++ src/client/widgets/chooser.h
@@ -1,14 +1,50 @@
-//
-// Created by derovi on 5/1/2020.
-//
-
 #ifndef CHOOSER_H
 #define CHOOSER_H
 
+#include <QApplication>
+#include <QRect>
 
-class Chooser {
+#include "../widget.h"
+#include "imagebutton.h"
 
+namespace client {
+
+class Chooser : public Widget {
+  public:
+    Chooser(QPoint position, int height, int width);
+
+    void paint(QPainter& painter) override;
+
+    int getSelected() const;
+
+    const std::shared_ptr<ImageButton>& getLeftButton() const;
+
+    const std::shared_ptr<ImageButton>& getRightButton() const;
+
+    const std::shared_ptr<TextView>& getTextView() const;
+
+  private:
+    void leftClick();
+
+    void rightClick();
+
+    QStringList options;
+
+    QImage background;
+
+    std::function<void(int selected)> onChanged;
+
+    std::shared_ptr<ImageButton> leftButton;
+
+    std::shared_ptr<ImageButton> rightButton;
+
+    std::shared_ptr<TextView> textView;
+
+    int textWidth;
+
+    int selected;
 };
 
+}
 
-#endif //CHOOSER_H
+#endif  // CHOOSER_H
