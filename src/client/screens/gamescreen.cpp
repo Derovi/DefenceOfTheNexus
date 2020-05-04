bbb8068c69046a8493d25cc0cc3e077f117f2e87
derovi <devderovi@gmail.com>
2020-05-04 23:37:18 +0300

    Codestyle fix

--- src/client/screens/gamescreen.cpp
+++ src/client/screens/gamescreen.cpp
@@ -82,8 +82,8 @@ client::GameScreen::GameScreen(const std::shared_ptr<core::GameWorld>& savedGame
     engine->start();
 
 
-    interface = new GameInterface(QPoint(0, 1500), gameMap->getGameWorld());
-    addChild(interface);
+    //interface = new GameInterface(QPoint(0, 1500), gameMap->getGameWorld());
+    //addChild(interface);
 }
 
 const std::shared_ptr<server::Engine>& client::GameScreen::getEngine() const {
