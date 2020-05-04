da1f21d60fd2673e6ad09b647562d6a5e1432bdb
derovi <devderovi@gmail.com>
2020-05-04 23:56:58 +0300

    Added heathbar cpp

---
+++ src/client/widgets/healthbar.cpp
@@ -0,0 +1,61 @@
+#include <QDebug>
+#include "healthbar.h"
+
+client::HealthBar::HealthBar(QPoint position, int currentHP, int maxHP) : Widget(position) {
+    setCurrentHp(currentHP);
+    setMaxHp(maxHP);
+}
+
+int client::HealthBar::getCurrentHp() const {
+    return currentHP;
+}
+
+void client::HealthBar::setCurrentHp(int currentHp) {
+    this->currentHP = currentHp;
+}
+
+int client::HealthBar::getMaxHp() const {
+    return maxHP;
+}
+
+void client::HealthBar::setMaxHp(int maxHp) {
+    this->maxHP = maxHp;
+}
+
+const QImage& client::HealthBar::getBackground() const {
+    return background;
+}
+
+void client::HealthBar::setBackground(const QImage& background) {
+    HealthBar::background = background;
+}
+
+const QImage& client::HealthBar::getHealthLine() const {
+    return healthLine;
+}
+
+void client::HealthBar::setHealthLine(const QImage& healthLine) {
+    HealthBar::healthLine = healthLine;
+}
+
+int client::HealthBar::getWidth() const {
+    return width;
+}
+
+void client::HealthBar::setWidth(int width) {
+    HealthBar::width = width;
+}
+
+int client::HealthBar::getHeight() const {
+    return height;
+}
+
+void client::HealthBar::setHeight(int height) {
+    this->height = height;
+}
+
+void client::HealthBar::paint(QPainter& painter) {
+    painter.drawImage(QRect(1296, 1593, 1250, 70), background);
+    QRect rect(0, 0, width * currentHP / maxHP, 50);
+    painter.drawImage(1306, 1603, healthLine.copy(rect));
+}
