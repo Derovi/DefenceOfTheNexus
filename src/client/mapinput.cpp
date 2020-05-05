c27a782700b2962e60afc9f464c8c57274906e75
derovi <devderovi@gmail.com>
2020-05-05 17:05:38 +0300

    Map input added

--- src/client/mapinput.cpp
+++ src/client/mapinput.cpp
@@ -1,1 +1,45 @@
+#include "../utils/factory.h"
+
 #include "mapinput.h"
+#include "../core/attributes/wall.h"
+
+client::MapInput::MapInput(client::GameMap* gameMap): gameMap(gameMap) {}
+
+void client::MapInput::abort() {
+    points.clear();
+    objectType.clear();
+}
+
+bool client::MapInput::isWaiting() {
+    return !objectType.isEmpty();
+}
+
+const QString& client::MapInput::getObjectType() const {
+    return objectType;
+}
+
+const QVector<QPoint>& client::MapInput::getPoints() const {
+    return points;
+}
+
+client::GameMap* client::MapInput::getGameMap() const {
+    return gameMap;
+}
+
+void client::MapInput::setObjectType(const QString& objectType) {
+    MapInput::objectType = objectType;
+}
+
+void client::MapInput::append(const QPoint& point) {
+    points.push_back(point);
+    auto objectSignature = utils::Factory::getObjectSignature(objectType);
+    if (!objectSignature) {
+        abort();
+        return;
+    }
+    if (objectSignature->hasAttribute(core::Wall::attributeName)) {
+
+    } else {
+
+    }
+}
--- src/client/mapinput.h
+++ src/client/mapinput.h
@@ -1,10 +1,39 @@
 #ifndef MAPINPUT_H
 #define MAPINPUT_H
 
+#include <QtCore/QString>
+#include <QtCore/QPoint>
+
+#include "widgets/gamemap.h"
+
+namespace client {
 
 class MapInput {
+  public:
+    explicit MapInput(GameMap* gameMap);
 
+    bool isWaiting();
+
+    void abort();
+
+    void append(const QPoint& point);
+
+    void setObjectType(const QString& objectType);
+
+    const QString& getObjectType() const;
+
+    const QVector<QPoint>& getPoints() const;
+
+    GameMap* getGameMap() const;
+
+  private:
+    QString objectType;
+
+    QVector<QPoint> points;
+
+    GameMap* gameMap;
 };
 
+}
 
-#endif //MAPINPUT_H
+#endif  // MAPINPUT_H
