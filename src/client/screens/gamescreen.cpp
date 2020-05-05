b84b375c72fb95be7866ec72bf0c14fe42d0e06d
TerNerss <mexz.5200@gmail.com>
2020-05-05 17:20:05 +0300

    Disposed resources, buildings, towers on GameMap

--- src/client/screens/gamescreen.cpp
+++ src/client/screens/gamescreen.cpp
@@ -54,30 +54,178 @@ client::GameScreen::GameScreen(const std::shared_ptr<core::GameWorld>& savedGame
         engine->setGameWorld(savedGameWorld);
     } else {
         engine->getGameWorld()->summonObject(utils::Factory::getObjectSignature("test1").value(),
-                                             QPoint(2000, 1000));
+                                             QPoint(1800, 1200));
 
         engine->getGameWorld()->summonObject(
                 utils::Factory::getObjectSignature("iron").value(),
-                QPoint(2200, 1000));
+                QPoint(842, 1422));
 
-        engine->getGameWorld()->buildWall(QPoint(0, 1500), QPoint(2000, 500),
-                                          utils::Factory::getObjectSignature("wall1").value(),
-                                          utils::Factory::getObjectSignature("column1").value());
-        engine->getGameWorld()->buildWall(QPoint(0, 1500), QPoint(-100, 50),
-                                          utils::Factory::getObjectSignature("wall1").value(),
-                                          utils::Factory::getObjectSignature("column1").value());
-        engine->getGameWorld()->buildWall(QPoint(2000, 1500), QPoint(0, 500),
-                                          utils::Factory::getObjectSignature("wall1").value(),
-                                          utils::Factory::getObjectSignature("column1").value());
-        engine->getGameWorld()->buildWall(QPoint(500, 1500), QPoint(0, 500),
-                                          utils::Factory::getObjectSignature("wall1").value(),
-                                          utils::Factory::getObjectSignature("column1").value());
-        engine->getGameWorld()->buildWall(QPoint(200, 200), QPoint(800, 800),
-                                          utils::Factory::getObjectSignature("wall1").value(),
-                                          utils::Factory::getObjectSignature("column1").value());
-        engine->getGameWorld()->buildWall(QPoint(-100, 500), QPoint(3, 9000),
-                                          utils::Factory::getObjectSignature("wall1").value(),
-                                          utils::Factory::getObjectSignature("column1").value());
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("iron").value(),
+                QPoint(3216, 566));
+
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("ore").value(),
+                QPoint(272, 324));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("ore").value(),
+                QPoint(3036, 1912));
+
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("stone").value(),
+                QPoint(3536, 1400));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("cobblestone").value(),
+                QPoint(3210, 270));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("rock").value(),
+                QPoint(932, 1960));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("small-stone").value(),
+                QPoint(1300, 622));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("medium-stone").value(),
+                QPoint(1600, 2004));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("medium-stone").value(),
+                QPoint(1850, 254));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("big-stone").value(),
+                QPoint(672, 468));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("big-stone").value(),
+                QPoint(340, 1570));
+
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("oak").value(),
+                QPoint(1130, 218));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("oak").value(),
+                QPoint(180, 1982));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("fir").value(),
+                QPoint(444, 1052));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("fir").value(),
+                QPoint(3182, 1200));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("conifer").value(),
+                QPoint(2616, 262));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("conifer").value(),
+                QPoint(2188, 1984));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("wite").value(),
+                QPoint(1280, 960));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("wite").value(),
+                QPoint(3688, 2040));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("nexus").value(),
+                QPoint(1920, 1104));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("gover").value(),
+                QPoint(2278, 1060));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("big-barrack").value(),
+                QPoint(1800, 1420));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("medium-barrack").value(),
+                QPoint(1730, 806));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("resource-bundle").value(),
+                QPoint(450, 236));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("dirt-rock").value(),
+                QPoint(1283, 1800));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("dirt-rock").value(),
+                QPoint(3010, 1670));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("grass").value(),
+                QPoint(1118, 1226));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("grass").value(),
+                QPoint(2642, 1494));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("grass").value(),
+                QPoint(2478, 650));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("grass").value(),
+                QPoint(1650, 466));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("big-tower").value(),
+                QPoint(2010, 614));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("medium-tower").value(),
+                QPoint(2077, 1666), 180);
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("small-tower").value(),
+                QPoint(198, 504), 45);
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("long-tower").value(),
+                QPoint(1596, 1648));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("special-tower").value(),
+                QPoint(2540, 1640));
+
+        engine->getGameWorld()->summonObject(
+                utils::Factory::getObjectSignature("high-tower").value(),
+                QPoint(1610, 592));
+
+
+//        engine->getGameWorld()->buildWall(QPoint(0, 1500), QPoint(2000, 500),
+//                                          utils::Factory::getObjectSignature("wall1").value(),
+//                                          utils::Factory::getObjectSignature("column1").value());
+//        engine->getGameWorld()->buildWall(QPoint(0, 1500), QPoint(-100, 50),
+//                                          utils::Factory::getObjectSignature("wall1").value(),
+//                                          utils::Factory::getObjectSignature("column1").value());
+//        engine->getGameWorld()->buildWall(QPoint(2000, 1500), QPoint(0, 500),
+//                                          utils::Factory::getObjectSignature("wall1").value(),
+//                                          utils::Factory::getObjectSignature("column1").value());
+//        engine->getGameWorld()->buildWall(QPoint(500, 1500), QPoint(0, 500),
+//                                          utils::Factory::getObjectSignature("wall1").value(),
+//                                          utils::Factory::getObjectSignature("column1").value());
+//        engine->getGameWorld()->buildWall(QPoint(200, 200), QPoint(800, 800),
+//                                          utils::Factory::getObjectSignature("wall1").value(),
+//                                          utils::Factory::getObjectSignature("column1").value());
+//        engine->getGameWorld()->buildWall(QPoint(-100, 500), QPoint(3, 9000),
+//                                          utils::Factory::getObjectSignature("wall1").value(),
+//                                          utils::Factory::getObjectSignature("column1").value());
     }
 
     gameMap->setGameWorld(engine->getGameWorld());
